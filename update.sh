8a1d8a4ad38ae62a5e77bc48c49656a15d869381
evan <evanhuang@staritgp.com>
2022-01-07 11:49:48 +0800

    change

--- update.sh
+++ update.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 
-docker build -f Dockerfile.build -t hello-world:build .
+docker build -f Dockerfile.build -t orchid-orchid-hello-world:build .
 
 find */ \( -name hello -or -name hello.txt \) -delete
-docker run --rm hello-world:build sh -c 'find \( -name hello -or -name hello.txt \) -print0 | xargs -0 tar --create' | tar --extract --verbose
+docker run --rm orchid-hello-world:build sh -c 'find \( -name hello -or -name hello.txt \) -print0 | xargs -0 tar --create' | tar --extract --verbose
 
 find -name hello -type f -exec dirname '{}' ';' | xargs -n1 -i'{}' cp Dockerfile-linux.template '{}/Dockerfile'
 find -name hello.txt -type f -exec dirname '{}' ';' | xargs -n1 -i'{}' cp Dockerfile-windows.template '{}/Dockerfile'
@@ -24,8 +24,8 @@ for h in amd64/*/hello; do
 	d="$(dirname "$h")"
 	b="$(basename "$d")"
 	"$h" > /dev/null
-	docker build -t hello-world:"test-$b" "$d"
-	docker run --rm hello-world:"test-$b"
+	docker build -t orchid-hello-world:"test-$b" "$d"
+	docker run --rm orchid-hello-world:"test-$b"
 done
 
 ls -lh */*/{hello,nanoserver*/hello.txt} || :
